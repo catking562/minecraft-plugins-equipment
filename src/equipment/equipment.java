2bfdf9adac17a867cb8542fcb3577097c0dc37fd
catking562 <taewookim562@gmail.com>
2021-09-06 22:55:10 +0900

    system

--- src/equipment/equipment.java
+++ src/equipment/equipment.java
@@ -94,6 +94,41 @@ public class equipment {
 		for(Entry<String, intdistence> entry : defaultAttributable.entrySet()) {
 			lore.add(main.getString(entry.getKey()).replace("<Value>", entry.getValue().getmax() + ""));
 		}
+		lore.add(main.getString("강화횟수").replace("<Value>", maxtighten + "").replace("<MaxValue>", maxtighten + ""));
+		if(level == 0) {
+			lore.add(main.getString("일반등급"));
+		}else if(level == 1) {
+			lore.add(main.getString("희귀등급"));
+		}else if(level == 2) {
+			lore.add(main.getString("레어등급"));
+		}else if(level == 3) {
+			lore.add(main.getString("에픽등급"));
+		}else if(level == 4) {
+			lore.add(main.getString("레전드등급"));
+		}
+		m.setLore(Arrays.asList(getlore()));
+		i.setItemMeta(m);
+		return i;
+	}
+	
+	public ItemStack getitemsystem() {
+		ItemStack i = new ItemStack(itemmaterial);
+		ItemMeta m = i.getItemMeta();
+		m.setDisplayName(getname());
+		ArrayList<String> lore = new ArrayList<String>();
+		lore.add(getlore());
+		if(equipmentcategorie.equalsIgnoreCase("무기")) {
+			lore.add(main.getString("무기분류"));
+		}else if(equipmentcategorie.equalsIgnoreCase("장비")) {
+			lore.add(main.getString("장비분류"));
+		}else if(equipmentcategorie.equalsIgnoreCase("장신구")) {
+			lore.add(main.getString("장신구분류"));
+		}else {
+			lore.add(main.getString("분류"));
+		}
+		for(Entry<String, intdistence> entry : defaultAttributable.entrySet()) {
+			lore.add(main.getString(entry.getKey()).replace("<Value>", entry.getValue().getmin() + "~" + entry.getValue().getmax()));
+		}
 		lore.add(main.getString("강화횟수").replace("<Value>", "0").replace("<MaxValue>", maxtighten + ""));
 		if(level == 0) {
 			lore.add(main.getString("일반등급"));
@@ -145,4 +180,44 @@ public class equipment {
 		i.setItemMeta(m);
 		return i;
 	}*/
+	
+	public void setname(String name) {
+		this.name = name;
+	}
+	
+	public void setlevel(int level) {
+		this.level = level;
+	}
+	
+	public void setmaxtighten(int maxtighten) {
+		this.maxtighten = maxtighten;
+	}
+	
+	public void setequipmentcategorie(String equipmentcategorie) {
+		this.equipmentcategorie = equipmentcategorie;
+	}
+	
+	public void setlore(String lore) {
+		this.lore = lore;
+	}
+	
+	public void setitemmaterial(Material itemmaterial) {
+		this.itemmaterial = itemmaterial;
+	}
+	
+	public void setdefaultAttributable(String string, intdistence i) {
+		if(i.getmin() == i.getmax() && i.getmax() == 0) {
+			defaultAttributable.put(string, null);
+		}else {
+			defaultAttributable.put(string, i);
+		}
+	}
+	
+	public void settightenAttributable(String string, intdistence i) {
+		if(i.getmin() == i.getmax() && i.getmax() == 0) {
+			tightenAttributable.put(string, null);
+		}else {
+			tightenAttributable.put(string, i);
+		}
+	}
 }
--- src/equipment/main.java
+++ src/equipment/main.java
@@ -2,13 +2,20 @@ package equipment;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.world.WorldSaveEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class main extends JavaPlugin {
@@ -16,6 +23,7 @@ public class main extends JavaPlugin {
 	Map<String, File> file = new HashMap<String, File>();
 	Map<String, YamlConfiguration> config = new HashMap<String, YamlConfiguration>();
     static Map<String, String> message = new HashMap<String, String>();
+    Map<String, equipment> equipmentlist = new HashMap<String, equipment>();
 	
 	public void messageconfig() {
 		File file = this.file.get("message");
@@ -77,6 +85,45 @@ public class main extends JavaPlugin {
 		if(config.getString("개발중") != null) {
 			config.set("개발중", "아직... 개발중이에요!");
 		}
+		if(config.getString("없는장비") != null) {
+			config.set("없는장비", "존재하지 않는 장비입니다.");
+		}
+		if(config.getString("있는장비") != null) {
+			config.set("있는장비", "이미 존재하는 장비입니다.");
+		}
+		if(config.getString("생성완료") != null) {
+			config.set("생성완료", "장비를 생성하였습니다.");
+		}
+		if(config.getString("삭제완료") != null) {
+			config.set("삭제완료", "장비를 삭제하였습니다.");
+		}
+		if(config.getString("이름설정완료") != null) {
+			config.set("이름설정완료", "장비의 이름을 설정하였습니다.");
+		}
+		if(config.getString("설명설정완료") != null) {
+			config.set("설명설정완료", "장비의 설명을 설정하였습니다.");
+		}
+		if(config.getString("정수입력") != null) {
+			config.set("정수입력", "정수를 입력해주세요.");
+		}
+		if(config.getString("아이템설정완료") != null) {
+			config.set("아이템설정완료", "장비의 아이템을 설정하였습니다.");
+		}
+		if(config.getString("등급설정완료") != null) {
+			config.set("등급설정완료", "장비의 등급을 설정하였습니다.");
+		}
+		if(config.getString("0~4") != null) {
+			config.set("0~4", "0 ~ 4의 정수만 입력하세요.");
+		}
+		if(config.getString("최대강화횟수설정완료") != null) {
+			config.set("최대강화횟수설정완료", "장비의 최대강화횟수를 설정하였습니다.");
+		}
+		if(config.getString("0이상") != null) {
+			config.set("0이상", "0이상의 정수만 입력해주세요");
+		}
+		if(config.getString("템지급") != null) {
+			config.set("템지급", "장비를 지급하였습니다.");
+		}
 		for(String key : config.getKeys(false)) {
 			this.message.put(key, config.getString(key));
 		}
@@ -109,6 +156,45 @@ public class main extends JavaPlugin {
 		return main.message.get(string).replace("&", "§");
 	}
 	
+	public void onDisable() {
+		save();
+	}
+	
+	public boolean isequipment(String string) {
+		for(Entry<String, equipment> entry : equipmentlist.entrySet()) {
+			if(entry.getKey().equalsIgnoreCase(string)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public equipment getequipment(String string) {
+		for(Entry<String, equipment> entry : equipmentlist.entrySet()) {
+			if(entry.getKey().equalsIgnoreCase(string)) {
+				return entry.getValue();
+			}
+		}
+		return null;
+	}
+	
+	public boolean isint(String string) {
+		try {
+			Integer.parseInt(string);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+	
+	public intdistence stringtodisint(String string) {
+		String[] s = string.split("~");
+		if(isint(s[0]) && isint(s[s.length])) {
+			return new intdistence(Integer.parseInt(s[0]), Integer.parseInt(s[s.length]));
+		}
+		return null;
+	}
+	
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		if(sender instanceof Player) {
@@ -119,7 +205,148 @@ public class main extends JavaPlugin {
 		    	p.sendMessage(getString("접두어") + getString("개발중"));
 		    }else if(command.getName().equalsIgnoreCase("장비설정")) {
 		    	if(args.length == 0) {
-		    		
+		    		p.sendMessage(getString("접두어") + "/장비설정 <이름>  --  장비를 확인합니다.");
+		    		p.sendMessage(getString("접두어") + "/장비설정 <이름> 생성  --  장비를 생성합니다.");
+		    		p.sendMessage(getString("접두어") + "/장비설정 <이름> 삭제  --  장비를 삭제합니다.");
+		    		p.sendMessage(getString("접두어") + "/장비설정 <이름> 이름 <display>  --  장비의 이름을 설정합니다.");
+		    		p.sendMessage(getString("접두어") + "/장비설정 <이름> 설명 <설명>  --  장비에 설명을 추가한다.");
+		    		p.sendMessage(getString("접두어") + "/장비설정 <이름> 아이템 <코드>  --  장비의 아이템을 설정한다.");
+		    		p.sendMessage(getString("접두어") + "/장비설정 <이름> 등급 <등급>  --  장비의 등급을 설정합니다");
+		    		p.sendMessage(getString("접두어") + "/장비설정 <이름> 기본능력치 <능력치> <값>  --  장비의 기본능력치를 설정한다.");
+		    		p.sendMessage(getString("접두어") + "/장비설정 <이름> 강화최대횟수 <값>  --  장비를 강화할 수 있는 최대 횟수를 설정한다.");
+		    		p.sendMessage(getString("접두어") + "/장비설정 <이름> 강화설정 <능력치> <값>  --  장비를 강화할때마다 해당 능력치가 얼마씩 증가할지 설정한다");
+		    		p.sendMessage(getString("접두어") + "/장비설정 <이름> 받기 <장비상태>  --  장비을 받는다");
+		    	}else if(args.length == 1) {
+		    		if(isequipment(args[0])) {
+		    			ItemStack i = getequipment(args[0]).getitemsystem();
+		    			ItemMeta m = i.getItemMeta();
+		    			p.sendMessage(m.getDisplayName());
+		    			for(String string : m.getLore()) {
+		    				p.sendMessage(string);
+		    			}
+		    		}else {
+		    			p.sendMessage(getString("접두어") + getString("없는장비"));
+		    		}
+		    	}else if(args.length == 2) {
+		    		if(isequipment(args[0])) {
+                        if(args[1].toString().equalsIgnoreCase("생성")) {
+		    				p.sendMessage(getString("접두어") + getString("있는장비"));
+		    			}else if(args[1].toString().equalsIgnoreCase("삭제")) {
+		    				equipmentlist.put(args[1], null);
+		    				p.sendMessage(getString("접두어") + getString("삭제완료"));
+		    			}else if(args[1].toString().equalsIgnoreCase("이름")) {
+		    				p.sendMessage(getString("접두어") + "/장비설정 <이름> 이름 <display>  --  장비의 이름을 설정합니다.");
+		    			}else if(args[1].toString().equalsIgnoreCase("설명")) {
+		    				p.sendMessage(getString("접두어") + "/장비설정 <이름> 설명 <설명>  --  장비에 설명을 추가한다.");
+		    			}else if(args[1].toString().equalsIgnoreCase("아이템")) {
+		    				p.sendMessage(getString("접두어") + "/장비설정 <이름> 아이템 <코드>  --  장비의 아이템을 설정한다.");
+		    			}else if(args[1].toString().equalsIgnoreCase("등급")) {
+		    				p.sendMessage(getString("접두어") + "/장비설정 <이름> 등급 <등급>  --  장비의 등급을 설정합니다");
+		    			}else if(args[1].toString().equalsIgnoreCase("기본능력치")) {
+		    				p.sendMessage(getString("접두어") + "/장비설정 <이름> 기본능력치 <능력치> <값>  --  장비의 기본능력치를 설정한다.");
+		    			}else if(args[1].toString().equalsIgnoreCase("강화최대횟수")) {
+		    				p.sendMessage(getString("접두어") + "/장비설정 <이름> 강화최대횟수 <값>  --  장비를 강화할 수 있는 최대 횟수를 설정한다.");
+		    			}else if(args[1].toString().equalsIgnoreCase("강화설정")) {
+		    				p.sendMessage(getString("접두어") + "/장비설정 <이름> 강화설정 <능력치> <값>  --  장비를 강화할때마다 해당 능력치가 얼마씩 증가할지 설정한다");
+		    			}else if(args[1].toString().equalsIgnoreCase("받기")) {
+		    				p.sendMessage(getString("접두어") + "/장비설정 <이름> 받기 <장비상태>  --  장비을 받는다");
+		    			}else {
+		    				p.sendMessage(getString("접두어") + getString("명령어오류"));
+		    			}
+		    		}else {
+                        if(args[1].toString().equalsIgnoreCase("생성")) {
+		    				equipment equi = new equipment();
+		    				equipmentlist.put(args[1].toString(), equi);
+		    			}else {
+		    				p.sendMessage(getString("접두어") + getString("없는장비"));
+		    			}
+		    		}
+		    	}else if(args.length == 3) {
+		    		if(isequipment(args[0])) {
+		    			if(args[1].toString().equalsIgnoreCase("이름")) {
+		    				equipmentlist.get(args[1].toString()).setname(args[2].toString());
+		    				p.sendMessage(getString("접두어") + getString("이름설정완료"));
+		    			}else if(args[1].toString().equalsIgnoreCase("설명")) {
+		    				equipmentlist.get(args[1]).setlore(args[2].toString());
+		    				p.sendMessage(getString("접두어") + getString("설명설정완료"));
+		    			}else if(args[1].toString().equalsIgnoreCase("아이템")) {
+		    				if(isint(args[2])) {
+		    					equipmentlist.get(args[1]).setitemmaterial(Material.getMaterial(Integer.parseInt(args[2])));
+		    					p.sendMessage(getString("접두어") + getString("아이템설정완료"));
+		    				}else {
+		    					p.sendMessage(getString("접두어") + getString("정수입력"));
+		    				}
+		    			}else if(args[1].toString().equalsIgnoreCase("등급")) {
+		    				if(isint(args[2])) {
+		    					if(Integer.parseInt(args[2]) >= 0 && Integer.parseInt(args[2]) <= 4) {
+		    						p.sendMessage(getString("접두어") + getString("등급설정완료"));
+		    					}else {
+		    						p.sendMessage(getString("접두어") + getString("0~4"));
+		    					}
+		    				}else {
+		    					p.sendMessage(getString("접두어") + getString("정수입력"));
+		    				}
+		    			}else if(args[1].toString().equalsIgnoreCase("기본능력치")) {
+		    				p.sendMessage(getString("접두어") + "/장비설정 <이름> 기본능력치 <능력치> <값>  --  장비의 기본능력치를 설정한다.");
+		    			}else if(args[1].toString().equalsIgnoreCase("강화최대횟수")) {
+		    				if(isint(args[2])) {
+		    					if(Integer.parseInt(args[2]) >= 0) {
+		    						equipmentlist.get(args[1]).setmaxtighten(Integer.parseInt(args[2]));
+		    						p.sendMessage(getString("접두어") + getString("강화최대횟수설정완료"));
+		    					}else {
+		    						p.sendMessage(getString("접두어") + getString("0이상"));
+		    					}
+		    				}else {
+		    					p.sendMessage(getString("접두어") + getString("정수입력"));
+		    				}
+		    			}else if(args[1].toString().equalsIgnoreCase("강화설정")) {
+		    				p.sendMessage(getString("접두어") + "/장비설정 <이름> 강화설정 <능력치> <값>  --  장비를 강화할때마다 해당 능력치가 얼마씩 증가할지 설정한다");
+		    			}else if(args[1].toString().equalsIgnoreCase("받기")) {
+		    				if(args[2].equalsIgnoreCase("일반")) {
+		    					p.getInventory().addItem(equipmentlist.get(args[1]).getitem());
+		    					p.sendMessage(getString("접두어") + getString("템지급"));
+		    				}else if(args[2].equalsIgnoreCase("풀일반")) {
+		    					p.getInventory().addItem(equipmentlist.get(args[1]).getitemfull());
+		    					p.sendMessage(getString("접두어") + getString("템지급"));
+		    				}else {
+		    					p.sendMessage(getString("접두어") + "장비상태 = {일반, 풀일반}");
+		    				}
+		    			}else {
+		    				p.sendMessage(getString("접두어") + getString("명령어오류"));
+		    			}
+		    		}else {
+                        if(args[1].toString().equalsIgnoreCase("생성")) {
+		    				p.sendMessage(getString("접두어") + getString("명령어오류"));
+		    			}else {
+		    				p.sendMessage(getString("접두어") + getString("없는장비"));
+		    			}
+		    		}
+		    	}else if(args.length == 4) {
+		    		if(isequipment(args[0])) {
+		    			if(args[1].toString().equalsIgnoreCase("이름")) {
+		    				equipmentlist.get(args[1].toString()).setname(args[2].toString() + args[3].toString());
+		    				p.sendMessage(getString("접두어") + getString("이름설정완료"));
+		    			}else if(args[1].toString().equalsIgnoreCase("설명")) {
+		    				equipmentlist.get(args[1]).setlore(args[2].toString() + args[3].toString());
+		    				p.sendMessage(getString("접두어") + getString("설명설정완료"));
+		    			}else if(args[1].toString().equalsIgnoreCase("기본능력치")) {
+		    				if(stringtodisint(args[3]) != null) {
+		    					equipmentlist.get(args[1]).setdefaultAttributable(args[2], stringtodisint(args[3]));
+		    				}
+		    			}else if(args[1].toString().equalsIgnoreCase("강화설정")) {
+		    				if(stringtodisint(args[3]) != null) {
+		    					equipmentlist.get(args[1]).settightenAttributable(args[2], stringtodisint(args[3]));
+		    				}
+		    			}else {
+		    				p.sendMessage(getString("접두어") + getString("명령어오류"));
+		    			}
+		    		}else {
+                        if(args[1].toString().equalsIgnoreCase("생성")) {
+		    				p.sendMessage(getString("접두어") + getString("명령어오류"));
+		    			}else {
+		    				p.sendMessage(getString("접두어") + getString("없는장비"));
+		    			}
+		    		}
 		    	}
 		    }else {
 		    	p.sendMessage(getString("접두어") + getString("명령어오류"));
@@ -128,6 +355,11 @@ public class main extends JavaPlugin {
 			System.out.println("플레이어만 입력할 수 있는 명령어입니다.");
 		}
 		return super.onCommand(sender, command, label, args);
+	}
+	
+	@EventHandler
+	public void autosave(WorldSaveEvent e) {
+		save();
 	}
 
 }
